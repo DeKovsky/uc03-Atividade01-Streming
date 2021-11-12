f520c4c872fd6ffd1078505cd9075e47ba9a1058
DeKovsky <andrewkovsky.dev@outlook.com>
2021-11-12 10:25:53 -0300

    Streaming-Senha-SENAC

--- README.md.txt
+++ README.md.txt
@@ -1,1 +1,3 @@
 Criação de uma página básica de Streaming, SENAC.
+
+Criada pasta atividade 02 - para criar um sistema C# com gerador de senha para um usuário da página de streming.
---
+++ atividade02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Atividade_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeUsuario;
+            int anoNascimento, anoAtual, idadeUsuario;
+            anoAtual = 2021;
+            
+
+            Console.WriteLine("Seja Bem Vindo ao Gerador de Senha da sua nova Plataforma de Streming");
+            Console.WriteLine("Por gentileza, nos informe o seu nome?");
+            nomeUsuario = Console.ReadLine();
+            
+            Console.WriteLine("Em qual ano você nasceu?");
+            anoNascimento = int.Parse(Console.ReadLine());
+
+            idadeUsuario = anoAtual - anoNascimento;
+            
+            if (idadeUsuario < 18)
+            {
+                Console.WriteLine("A SENHA da sua nova Plataforma de Streaming é: {0}{1}",nomeUsuario,idadeUsuario);
+
+            }
+
+            else
+            {
+                
+                Console.WriteLine("A SENHA da sua nova Plataforma de Streaming é: {1}{0}",nomeUsuario,idadeUsuario);
+            }
+        }
+       
+
+    }
+}
